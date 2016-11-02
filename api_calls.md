fd67d54328f9413b45e1cb598e7a4a4b5795d1e0
Tommy Meisel <thomas.meisel95@gmail.com>
2016-11-02 15:09:36 -0500

    completed more api functions

--- api_calls.md
+++ api_calls.md
@@ -23,12 +23,16 @@ Node class
   - ref to root element (\_root)
 
 Done
+  - getNodeByPath(path) - return child with the path
   - getChildren() - return list of immediate children
+  - getChildren(metaType) - return list of immediate children of metaType
   - getAttribute(attributeName) - return attribute of node
   - getAttributeNames() - return list of attribute names
+  - getNodeByGuid(guid) - return child node with guid
   - getRelid() - return relative id of node
   - getGuid() - return guid of node
   - isMetaNode() - return if node is meta
+  - getAllMetaNodes() - return list of all meta nodes
 
   - ref to element in element tree (etree) (from xmi) (\_el)
   - ref to root element (\_root)
--- parseXMI.py
+++ parseXMI.py
@@ -1,9 +1,12 @@
 import xml.etree.ElementTree as ET
 
 class Node:
-    def __init__(self, el, root):
-        self._root = root
+    def __init__(self, el, root=None):
         self._el = el
+        if root:
+            self._root = root
+        else:
+            self._root = self
 
     def _elToNode(self, el):
         return Node(el, self._root)
@@ -12,41 +15,59 @@ class Node:
         return map(lambda x: self._elToNode(x), els)
 
     def _getElAttrib(self, el, attr):
-        return el.attrib.get(attr)
+        return el.get(attr)
 
     def _isElMeta(self, el):
         return self._getElAttrib(el, 'isMeta') == 'true'
 
-    # def _isElMetaType(self, el, metaType):
-        # return self._getElAttrib
+    def _getNodesByElAttrib(self, attrib, val):
+        nodes = filter(lambda x: self._getElAttrib(x, attrib) == val, self._el)
+        if (len(nodes) == 0):
+            return None
+        else:
+            return self._elsToNodes(nodes)
 
     def isMetaNode(self):
         return self._isElMeta(self._el)
 
     def getChildren(self, metaType=None):
         if metaType:
-            # return self._elsToNodes(filter(lambda x: self._isElMeta(x), els))
-            return []
+            # findall(tag) returns all immediate children with the given tag
+            return self._elsToNodes(self._el.findall(metaType))
         else:
             return self._elsToNodes(self._el)
 
     def getAttribute(self, attributeName):
-        return self._getElAttrib(self._el, attributeName)
+        return self._getElAttrib(self._el, 'atr-' + attributeName)
 
     def getAttributeNames(self):
-        return list(self._el.attrib)
+        return map(lambda x: x[4:], filter(lambda x: x.startswith('atr-'), self._el.attrib))
 
     def getRelid(self):
-        return self.getAttribute('relid')
+        return '/' + self._getElAttrib(self._el, 'relid')
 
     def getGuid(self):
-        return self.getAttribute('id')
+        return self._getElAttrib(self._el, 'id')
 
-def readFile(file):
-    tree = ET.parse(file)
-    return tree.getroot()
+    def getNodeByPath(self, path):
+        return self._getNodesByElAttrib('relid', path[1:])[0]
+
+    def getNodeByGuid(self, guid):
+        return self._getNodesByElAttrib('id', guid)[0]
+
+class Core:
+    def __init__(self, file):
+        tree = ET.parse(file)
+        self._el = tree.getroot()
+        self._root = Node(self._el)
+
+    def getRootNode(self):
+        return self._root
+
+    def getAllMetaNodes(self):
+        return map(lambda x: Node(x, self._root), filter(lambda x: x.get('atr-name') == x.tag, self._el.iter()))
 
 if __name__ == '__main__':
-    rootEl = readFile('FSMSignalFlow.xmi')
-    rootNode = Node(rootEl, None)
-    print rootNode.getChildren()[2].getGuid()
+    core = Core('FSMSignalFlow.xmi')
+    rootNode = core.getRootNode()
+    print core.getAllMetaNodes()
