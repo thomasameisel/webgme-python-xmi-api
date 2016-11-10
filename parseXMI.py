bbf6c9a3b625f5bdfa5a31721add50224a5f07a6
Tommy Meisel <thomas.meisel95@gmail.com>
2016-11-10 15:32:57 -0600

    added more methods, for get node by path and guid

--- parseXMI.py
+++ parseXMI.py
@@ -8,69 +8,110 @@ class Node:
         else:
             self._root = self
 
-    def _elToNode(self, el):
+    def _el_to_node(self, el):
         return Node(el, self._root)
 
-    def _elsToNodes(self, els):
-        return map(lambda x: self._elToNode(x), els)
+    def _els_to_nodes(self, els):
+        return map(lambda x: self._el_to_node(x), els)
 
-    def _getNodesByElAttrib(self, attrib, val):
+    def _get_children_by_el_attrib(self, attrib, val):
         nodes = filter(lambda x: x.get(attrib) == val, self._el)
-        return self._elsToNodes(nodes)
+        return self._els_to_nodes(nodes)
 
-    def isMetaNode(self):
+    def _get_el_by_relative_path_lst(self, el, relative_path_lst):
+        if (len(relative_path_lst) == 0):
+            return el
+        else:
+            for elChild in el:
+                if (elChild.get('relid') == relative_path_lst[0]):
+                    return self._get_el_by_relative_path_lst(elChild, relative_path_lst[1:])
+            return None
+
+    def _get_el_by_guid(self, el, guid):
+        for elChild in el:
+            if elChild.get('id') == guid:
+                return elChild
+            else:
+                potential_el = self._get_el_by_guid(elChild, guid)
+                if (potential_el is not None):
+                    return potential_el
+        return None
+
+    def _get_partial_path(self, end, nlist):
+        print self.get_relid(), end.get_relid()
+        if self.get_relid() == end.get_relid():
+            print "ab"
+            return nlist
+        children = self.get_children()
+        if children:
+            for child in children:
+                print "ac"
+                return child._get_partial_path(end, nlist+[self.get_relid()])
+        # else:
+        #     return None
+
+    def is_meta_node(self):
         return el.get('isMeta') == 'true'
 
-    def getChildren(self, metaType=None):
-        if metaType:
+    def get_children(self, meta_type=None):
+        if meta_type:
             # findall(tag) returns all immediate children with the given tag
-            return self._elsToNodes(self._el.findall(metaType))
+            return self._els_to_nodes(self._el.findall(meta_type))
         else:
-            return self._elsToNodes(self._el)
+            return self._els_to_nodes(self._el)
 
-    def getAttribute(self, attributeName):
-        return self._el.get('atr-' + attributeName)
+    def get_attribute(self, attribute_name):
+        return self._el.get('atr-' + attribute_name)
 
-    def getAttributeNames(self):
+    def get_attribute_names(self):
         return map(lambda x: x[4:], filter(lambda x: x.startswith('atr-'), self._el.attrib))
 
-    def getRelid(self):
+    def get_relid(self):
         relid = self._el.get('relid')
         if (relid is None):
             return None
         else:
             return '/' + self._el.get('relid')
 
-    def getGuid(self):
+    def get_guid(self):
         return self._el.get('id')
 
-    def getNodeByPath(self, path):
-        nodes = self._getNodesByElAttrib('relid', path[1:])
+    def get_node_by_relative_path(self, relative_path):
+        relative_path_lst = filter(lambda x: len(x) > 0, relative_path.split('/'))
+        el = self._get_el_by_relative_path_lst(self._el, relative_path_lst)
+        if (el is None):
+            return None
+        else:
+            return self._el_to_node(el)
+
+    def get_node_by_guid(self, guid):
+        el = self._get_el_by_guid(self._el, guid)
+        if (el is None):
+            return None
+        else:
+            return self._el_to_node(el)
+
+    def get_child_by_relid(self, relid):
+        nodes = self._get_children_by_el_attrib('relid', relid)
         if (len(nodes) == 0):
             return None
         else:
             return nodes[0]
 
-    def getNodeByGuid(self, guid):
-        nodes = self._getNodesByElAttrib('id', guid)
+    def get_child_by_guid(self, guid):
+        nodes = self._get_children_by_el_attrib('id', guid)
         if (len(nodes) == 0):
             return None
         else:
             return nodes[0]
 
-    def _getPartialPath(self, end, nlist):
-        print self.getRelid(), end.getRelid()
-        if self.getRelid() == end.getRelid():
-            print "ab"
-            return nlist
-        children = self.getChildren()
-        if children:
-            for child in children:
-                print "ac"
-                return child.getPartialPath(end, nlist+[self.getRelid()])
-        # else:
-        #     return None
-
+    def get_path(self):
+        children = self._root.get_children()
+        for child in children:
+            print child.get_relid()
+            path = child._get_partial_path(self, [child.get_relid()])
+            if path:
+                return path
 
 class Core:
     def __init__(self, file):
@@ -78,26 +119,24 @@ class Core:
         self._el = tree.getroot()
         self._root = Node(self._el)
 
-    def getRootNode(self):
+    def get_root_node(self):
         return self._root
 
-    def getAllMetaNodes(self):
+    def get_all_meta_nodes(self):
         return map(lambda x: Node(x, self._root), filter(lambda x: x.get('atr-name') == x.tag, self._el.iter()))
 
-    def getPath(self, node):
-        children = self._root.getChildren()
-        for child in children:
-            print child.getRelid()
-            path = child.getPartialPath(node, [child.getRelid()])
-            if path:
-                return path
+    def get_node_by_path(self, path):
+        return self._root.get_node_by_relative_path(path)
 
+    def get_node_by_guid(self, guid):
+        return self._root.get_node_by_guid(guid)
 
 if __name__ == '__main__':
     core = Core('FSMSignalFlow.xmi')
-    rootNode = core.getRootNode()
+    rootNode = core.get_root_node()
+    print rootNode.get_node_by_relative_path('/6/0')
     # print rootNode.getChildren()[1].getRelid()
-    anode = rootNode.getChildren()[1]
-    bnode = anode.getChildren()[1]
+    anode = rootNode.get_children()[1]
+    bnode = anode.get_children()[1]
     # print bnode.getRelid()
-    print core.getPath(bnode)
+    print bnode.get_path()
